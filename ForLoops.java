c2ea0c5b73543ba1f964695f21e85a2a7406d7d4
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 22:31:32 +0530

    Add files via upload

---
+++ ForLoops.java
@@ -0,0 +1,38 @@
+package loops;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+	
+////		for(int i = 0; i < 100; i = i + 1) {
+////			System.out.println("Hello World " + i);
+////		}
+////		for(int i = 0; i<=100 ; i = i+2) {
+////			System.out.println(i);
+////		}
+//		
+////		int sum = 0;  
+////		int n = 5;
+////		for(int i = 1; i<=n ; i++) {
+////			sum = sum + i;
+////	    }
+////	     
+////	    System.out.println(sum);
+//	    
+//	    for(int i = 100; i>0; i--) {
+//	    	System.out.println(i);
+////	    
+		
+//		}
+	
+	     int tableOf = 23;
+	     for(int i =1; i<=10; i++) {
+		     System.out.println(tableOf * i);
+	     } 
+	     
+	     for(;  ;) {
+	    	 System.out.println("hello");
+	     }
+	}
+	
+}
